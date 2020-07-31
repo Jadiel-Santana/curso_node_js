d190f8268d283de8f2dc36607963df206d04fa0f
Jadiel-Santana <jadiel.bsantana@gmail.com>
2020-07-31 01:37:07 -0300

    Adição do Nodemon e tratando os erros de url inválidas..

--- app.js
+++ app.js
@@ -3,5 +3,19 @@ const app = express()
 const routProducts = require('./routes/products')
 
 app.use('/products', routProducts)
+app.use((req, res, next) => {
+    const error = new Error('Não encontrado.')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    return res.send({
+        erro: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app
